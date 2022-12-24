520e0b57dbd495accf4e4f723a7a1e53a94b084e
mohamed <mlamine642@gmail.com>
2022-12-24 16:20:30 +0100

    add circuit breakers to order service

--- inventory-service/src/main/java/com/mohaDev/inventoryservice/Service/InventoryService.java
+++ inventory-service/src/main/java/com/mohaDev/inventoryservice/Service/InventoryService.java
@@ -4,16 +4,24 @@ import com.mohaDev.inventoryservice.DTO.InventoryTO;
 import com.mohaDev.inventoryservice.Mapper.InventoryMapper;
 import com.mohaDev.inventoryservice.Repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
    private final InventoryMapper inventoryMapper;
     private final InventoryRepository invevtoryRepository;
+    @SneakyThrows
     public List<InventoryTO> isInStock(List<String> skuCode){
+        log.info("start");
+        Thread.sleep(1000);
+        log.info("end");
+
        return invevtoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory
                -> inventoryMapper.to(inventory)).toList();
     }
--- order-service/src/main/java/com/moha/OrderService/Controller/OrderController.java
+++ order-service/src/main/java/com/moha/OrderService/Controller/OrderController.java
@@ -2,7 +2,11 @@ package com.moha.OrderService.Controller;
 
 import com.moha.OrderService.DTO.OrderDto;
 import com.moha.OrderService.Service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
    private final OrderService orderService;
     @PostMapping("/create")
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
+    //@TimeLimiter(name = "inventory" )
+   // @Retry(name = "inventory")
     public ResponseEntity<OrderDto> create(@RequestBody OrderDto orderDto) throws Exception {
         return ResponseEntity.ok(orderService.createOrder(orderDto));
+    }
+
+
+    public ResponseEntity<OrderDto> fallbackMethod(OrderDto orderDto ,RuntimeException runtimeException){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
     }
 
 }
